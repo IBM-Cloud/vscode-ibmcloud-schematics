a48d27acceb479c8f2a8b8b22e3dcc80247a2d6d
Soumyaraj-Biswal <soumyaraj.biswal@ibm.com>
2022-01-21 15:02:09 +0530

    ratecard integration

--- extension-ui/src/components/workspace/estimateCost/index.js
+++ extension-ui/src/components/workspace/estimateCost/index.js
@@ -34,9 +34,9 @@ const EstimateCost = ({ result }) => {
                             <StructuredListCell>{item.terraformItemId}</StructuredListCell>
                             <StructuredListCell>{item.id}</StructuredListCell>
                             <StructuredListCell>{item.title}</StructuredListCell>
-                            <StructuredListCell>{item.currlineitemtotal.toFixed(2)} {result?.currency}</StructuredListCell>
-                            <StructuredListCell>{item.prevlineitemtotal.toFixed(2)} {result?.currency}</StructuredListCell>
-                            <StructuredListCell>{item.changelineitemtotal.toFixed(2)} {result?.currency}</StructuredListCell>
+                            <StructuredListCell>{item.currlineitemtotal.toFixed(2)} {result?.currency}{item.rateCardCost?"*":""}</StructuredListCell>
+                            <StructuredListCell>{item.prevlineitemtotal.toFixed(2)} {result?.currency}{item.rateCardCost?"*":""}</StructuredListCell>
+                            <StructuredListCell>{item.changelineitemtotal.toFixed(2)} {result?.currency}{item.rateCardCost?"*":""}</StructuredListCell>
                         </StructuredListRow>
                         ))}
                         <StructuredListRow key={result?.Lineitem.length+1}></StructuredListRow>
@@ -45,8 +45,9 @@ const EstimateCost = ({ result }) => {
                                 direction="bottom"
                                 triggerText="Total estimated cost"
                             >
-                                <p style={{margin:'0',width:'300px',mixBlendMode: 'difference'}}>
+                                <p style={{padding:'2px',fontSize:"15px" ,width:'170px',background: '#0f62fe'}}>
                                     The cost displayed here are just an estimated cost not an actual cost.
+                                    * indicated cost has been derived from ratecard
                                 </p>
                             </Tooltip>
                         </StructuredListCell>
