6a30ba8236421ff9d69a6c94245fada2d727ab81
Vishwakumar Patil <vishwakumarp@gmail.com>
2022-05-06 10:09:27 +0530

    refactor: cost estimation (#36)
    export commands (#37)
    
    Co-authored-by: shipra-Jain1 <shipra.Jain1@ibm.com>
    
    refactor and add output channel
    
    fix error

--- extension-client/src/command/workspace/index.ts
+++ extension-client/src/command/workspace/index.ts
@@ -24,6 +24,7 @@ export * from './resources';
 export * from './log';
 export * from './job';
 export * from './variables';
+export * from './tfcost';
 
 import DetailsView from '../../webview/workspace/DetailsView';
 
